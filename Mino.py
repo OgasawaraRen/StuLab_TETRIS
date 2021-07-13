f3af61eef4d2f862b9f42c857d27308fded61cbc
Ren Ogasawara <e205701@ie.u-ryukyu.ac.jp>
2021-07-13 17:51:38 +0900

    仮決定

--- Mino.py
+++ Mino.py
@@ -1,9 +1,9 @@
 class Mino():
     def __init__(self):
-        self.shapes = []
-        self.x = 0
-        self.y = 0
-        self.rotateNum = 0
+        self.shapes = [] #ミノの形
+        self.x = 0 #x座標
+        self.y = 0 #y座標
+        self.rotateNum = 0 # 値×90度 時計回りに回転
     
     def rotateRight(self):
         self.rotateNum = (self.rotateNum+1)%4
