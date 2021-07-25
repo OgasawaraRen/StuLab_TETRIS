6af8daae4f074c5bac561480536e4e81488dbc19
Ren Ogasawara <e205701@ie.u-ryukyu.ac.jp>
2021-07-25 12:14:53 +0900

    ミノ出現位置を修正

--- Mino.py
+++ Mino.py
@@ -1,8 +1,10 @@
+import Const
+
 class Mino:
     def __init__(self,shapes):
         self.shapes = shapes #ミノの形
-        self.x = 0 #x座標
-        self.y = 0 #y座標
+        self.x = int(Const.BOARD_W/2) - len(shapes[0][0])#x座標
+        self.y = -2 #y座標
         self.rotateNum = 0 # 値×90度 時計回りに回転
     
     def rotateRight(self):
