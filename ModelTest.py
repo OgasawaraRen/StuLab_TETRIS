fb70e8cb263cdef70ee5dfc61544984442188dd8
Ren Ogasawara <e205701@ie.u-ryukyu.ac.jp>
2021-08-02 00:19:11 +0900

    テストコード1つ実装

--- ModelTest.py
+++ ModelTest.py
@@ -1,57 +1,39 @@
 import unittest
-import Model as md
 import numpy as np
-class Model:
+from Model import Score
+import random
+import Const
+class ModelDriver:
     def __init__(self) -> None:
         self.TRAP = 10
         self.BOARD_H = 20
         self.BOARD_W = 10
-        self.board = np.zeros([self.BOARD_H,self.BOARD_W])
+        self.board = np.zeros([self.BOARD_H,self.BOARD_W],dtype=int)
+        self.initTraps()
 
-    def checkGameOver(self)->bool:#3 ゲームオーバーだったらfalse,#ゲームオーバーか？
-        self.board = np.zeros([20,10])
-        def checkGameOver(self)->bool:#3 ゲームオーバーだったらfalse,#ゲームオーバーか？
-        check = np.count_nonzero(self.board != -1) #-1の数を数える
-        if check != self.TRAP:   
-            return False
-
-        for row in self.board:
-            if np.sum(row) == self.BOARD_W:
-                return False
-
-        return True
+    def initTraps(self):
+        #全ての列にトラップマスを用意する
+        traps = list(range(10))
+        random.shuffle(traps)
+        for i in range(int(Const.BOARD_H/2)): #二行ごとに一つのトラップマスを用意する
+            self.board[i*2,traps[i]] = -1
 
 class PerfectScoreTest(unittest.TestCase):
-    def testPerfectScore(self):
-        for i in range(self.BOARD_H):
-            if i%2 == 0:
-                for j in range(self.BOARD_W-1):
-                    md.Model.board[i,j] = 1
+    def test_Model(self):
+        md = ModelDriver()
+        self.board = md.board
+        for i in range(Const.BOARD_H):
+            if i%2 != 0:
+                for j in range(Const.BOARD_W-1):
+                    self.board[i,j] = 1
             else:
-                for j in range(self.BOARD_W):
-                    if md.Model.board[i,j] != 0:
-                        md.Model.board[i,j] = 1
-        md.Score.calcScore(self.board)
-        
-'''
-class ModelTest(unittest.TestCase):
-    def test_Model(self):
-        a = np.arange(200).reshape((20,10))
-        clear_board = a[0:,0:1] = 1
-        expected = "True"
-        actual = Model.checkGameOver(clear_board)
-        self.assertEqual(expected,actual)
-class ModelTest2(unittest.TestCase):
-    def test_Model(self):
-        miss_board = np.zeros((20,10))
-        expected = "False"
-        actual = Model.checkGameOver(miss_board)
+                for j in range(Const.BOARD_W):
+                    if self.board[i,j] != -1:
+                        self.board[i,j] = 1
+        print(self.board)
+        expected = 100.0
+        actual = Score.calcScore(self.board)
         self.assertEqual(expected,actual)
 
-class ModelTest3(unittest.TestCase):
-    def test_Model(self):
-        
-        self.assertEqual(expected,actual)
-'''
 if __name__ == "__main__":
     unittest.main()
